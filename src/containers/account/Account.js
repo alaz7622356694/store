0ded2a87f76e15695f2c1b9316d5796e2a664b71
alaz7622356694 <alireza.azari2235@outlook.com>
2021-06-11 17:46:45 +0430

    creating dynamic form based on state

--- src/containers/account/Account.js
+++ src/containers/account/Account.js
@@ -2,14 +2,59 @@ import React from 'react'
 import Input from '../../../src/components/ui elements/input/Input'
 import Button from '../../components/ui elements/button/Button'
 class Account extends React.Component {
+state={
+  form:{
+    name:{
+      elementType:'input',
+      elementConfig:{
+        type:'text',
+        placeholder:'name...'
+      },
+      value:''
+    },
+   
+    email:{
+      elementType:'input',
+      elementConfig:{
+        type:'text',
+        placeholder:'Email...'
+      },
+      value:''
+    },
+    password:{
+      elementType:'input',
+      elementConfig:{
+        type:'password',
+        placeholder:'password...'
+      },
+      value:''
+    },
+  }
+}
+
+
+
+
   render() {
+    const elementArray=[]
+    for(let item in this.state.form){
+      elementArray.push({
+        id:item,
+        config: this.state.form[item]
+      })
+    }
     return (
      <div className='account'>
 <h2>Account</h2>
 <form>
-    <Input type="text" placeholder="Name..."/>
-    <Input type="text" placeholder="Email"/>
-    <Input type="password" placeholder="password"/>
+   {elementArray.map((item)=>{
+     return(
+     <Input 
+     key={item.id} 
+     elementType={item.config.elementType}
+     elementConfig={item.config.elementConfig}
+     value={item.config.value} />)
+  })}
     <Button btnType='form'>Submit</Button>
 </form>
      </div>
